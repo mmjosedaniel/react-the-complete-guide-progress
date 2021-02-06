bbb95646de3a2e7e3f3ad8dea63ac3401ac46f01
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-06 17:16:13 -0500

    60. Flexible Lists

--- react-complete-gide/src/App.js
+++ react-complete-gide/src/App.js
@@ -13,14 +13,22 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({ 
-      persons: [
-        { name: 'max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id; 
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -55,6 +63,7 @@ class App extends Component {
               name={person.name} 
               age={person.age}
               key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)}
             />
           })}
         </div>
