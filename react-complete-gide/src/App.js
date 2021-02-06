3dea56a0e002dba022e67e63c3ec589b52ec8bdc
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-06 15:11:05 -0500

    59. Lists and Keys

--- react-complete-gide/src/App.js
+++ react-complete-gide/src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'abehd', name: 'Max', age: 28 },
+      { id: 'dbuda', name: 'Manu', age: 29 },
+      { id: 'xrhui', name: 'Stephanie', age: 26 }
     ],
     otherState: 'someOtherValue',
     showPersons: false
@@ -53,7 +53,8 @@ class App extends Component {
             return <Person 
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
-              age={person.age} 
+              age={person.age}
+              key={person.id}
             />
           })}
         </div>
