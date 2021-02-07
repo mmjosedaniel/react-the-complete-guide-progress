747c56caa2a8073c20250d26ecb01af6811a56cf
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-07 11:05:50 -0500

    66. Setting Styles Dynamically

--- react-complete-gide/src/App.js
+++ react-complete-gide/src/App.js
@@ -45,7 +45,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -68,6 +69,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
 
     return (
