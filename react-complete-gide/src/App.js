e15da7d033275a8699f1d2b2892c298fe6711b56
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-08 13:42:55 -0500

    68 Adding and Using Radium

--- react-complete-gide/src/App.js
+++ react-complete-gide/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
+
 import Person from './Person/Person'
 
 class App extends Component {
@@ -50,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -71,6 +77,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -99,4 +109,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
