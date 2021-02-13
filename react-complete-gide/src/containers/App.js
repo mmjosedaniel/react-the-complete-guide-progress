ab5365f222b0a4924427c53dd947a1f1321a0304
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-13 15:11:07 -0500

    85. A Better Project Structure

--- react-complete-gide/src/containers/App.js
+++ react-complete-gide/src/containers/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 import classes from "./App.css";
 
-import Person from "./Person/Person";
+import Person from "../components/Persons/Person/Person";
 
 class App extends Component {
   state = {
@@ -55,14 +54,13 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}>
                 <Person
                   click={() => this.deletePersonHandler(index)}
                   name={person.name}
                   age={person.age}
+                  key={person.id}
                   changed={(event) => this.nameChangeHandler(event, person.id)}
                 />
-              </ErrorBoundary>
             );
           })}
         </div>
