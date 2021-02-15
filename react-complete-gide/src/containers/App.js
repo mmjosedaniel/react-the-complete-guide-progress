42686f3ead9074bffc7dec0a298587a194d1401f
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-15 12:16:47 -0500

    92. Component Update Lifecycle (for State Changes)

--- react-complete-gide/src/containers/App.js
+++ react-complete-gide/src/containers/App.js
@@ -7,7 +7,7 @@ import classes from "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    console.log('[App.js] constructor');
+    console.log("[App.js] constructor");
   }
 
   state = {
@@ -21,17 +21,26 @@ class App extends Component {
   };
 
   static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
+    console.log("[App.js] getDerivedStateFromProps", props);
     return state;
-  };
+  }
 
   // Will be removed [deprecated]
   // componentWillMount(){
   //   console.log('[App.js] componentWillMonut');
   // }
 
-  componentDidMount(){
-    console.log('[App.js] componentDidMount');
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js] shouldComponentsUpdate");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("[App.js] componentDidUpdate");
   }
 
   nameChangeHandler = (event, id) => {
@@ -65,7 +74,7 @@ class App extends Component {
   };
 
   render() {
-    console.log('[App.js] render');
+    console.log("[App.js] render");
     let persons = null;
 
     if (this.state.showPersons) {
