6129f651fa33af2ed349cf1e280fb8614844d0aa
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-15 19:23:43 -0500

    93. Using useEffect() in Functionanl Component

--- react-complete-gide/src/components/Cockpit/Cockpit.js
+++ react-complete-gide/src/components/Cockpit/Cockpit.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+
+    //HTTP request..
+  });
+
   const assignedClasses = [];
   let btnClass = "";
   if (props.showPersons) {
