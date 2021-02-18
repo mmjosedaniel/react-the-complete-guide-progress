483c4a5466ca302d34e19028b9f17691d1f43a27
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-18 14:05:06 -0500

    Rendering Adjacent JSX Elements

--- react-complete-gide/src/components/Persons/Person/Person.js
+++ react-complete-gide/src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import Aux from "../../../hoc/Aux";
+
 import classes from "./Person.css";
 
 class Person extends Component {
@@ -7,13 +9,18 @@ class Person extends Component {
     console.log("[Person.js] rendering...");
 
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old{" "}
         </p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+        <p key="i2">{this.props.children}</p>
+        <input
+          key="i3"
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
     );
   }
 }
