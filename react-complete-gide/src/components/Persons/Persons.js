32b6aff74155eba73a0d7869e496c87a13dd00af
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-18 12:11:26 -0500

    100. PureComponents instead of shouldComponentUpdate

--- react-complete-gide/src/components/Persons/Persons.js
+++ react-complete-gide/src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import Person from "./Person/Person";
 
-class Persons extends Component {
+class Persons extends PureComponent {
   // static getDerivedStateFromProps(props, state) {
   //   console.log("[Persons.js] getDerivedStateFromProps");
   //   return state;
@@ -12,15 +12,19 @@ class Persons extends Component {
   //   console.log("[Persons.js] componentWillReceiveProps", props);
   // }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("[Persons.js] shouldComponentUpdate");
-    if(nextProps.persons !== this.props.persons) {
-      return true;
-    } else {
-      return false;
-    }
-    // return true;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("[Persons.js] shouldComponentUpdate");
+  //   if (
+  //     nextProps.persons !== this.props.persons ||
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.clicked !== this.props.clicked
+  //   ) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  //   // return true;
+  // }
 
   getSnapshotBeforeUpdate(prevProps, PrevState) {
     console.log("[Persons.js] getSnapShotBeforeUpdate");
@@ -37,7 +41,7 @@ class Persons extends Component {
   }
 
   componentWillUnmount() {
-    console.log('[Persons.js] componentWillUnmount');
+    console.log("[Persons.js] componentWillUnmount");
   }
 
   render() {
