7d825a8b60de155b623d2ee3cc6df5afd36a91cf
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-18 20:07:11 -0500

    106. Another Form of HOCs

--- react-complete-gide/src/containers/App.js
+++ react-complete-gide/src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit.js";
-import WithClass from "../hoc/WithClass";
+import withClass from "../hoc/withClass";
+import Aux from "../hoc/Aux";
 
 import classes from "./App.css";
 
@@ -91,7 +92,7 @@ class App extends Component {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -108,10 +109,10 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </WithClass>
+      </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
