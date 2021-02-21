99f910af3b614c517555038dae963cecaaf18ecc
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-20 20:16:05 -0500

    114. contextType and useContext()

--- react-complete-gide/src/components/Persons/Person/Person.js
+++ react-complete-gide/src/components/Persons/Person/Person.js
@@ -13,6 +13,8 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // Next line selects the first imput element >>>
     // document.querySelector('input').focus();
@@ -20,6 +22,7 @@ class Person extends Component {
     // this.inputElement.focus();
 
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
@@ -27,11 +30,11 @@ class Person extends Component {
 
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {(context) =>
-            context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please log in</p>
+        )}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old{" "}
         </p>
