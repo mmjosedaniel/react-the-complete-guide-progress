8c7de0cb7b928ef860735d4ff2771270696321e9
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-23 21:17:23 -0500

    135. Displaying and Updating the Burger Price

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disableInfo}
+          price={this.state.totalPrice}
         />
       </Aux>
     );
