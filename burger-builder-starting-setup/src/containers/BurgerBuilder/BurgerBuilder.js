3c343bea409fd4e35a18951eb100fc23cae2fcf5
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-02-27 15:05:09 -0500

    141. Implementing the Button Component, Fix: Button.js join() method wrong positioned

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -81,6 +81,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
 
+  purchaseContinueHandler = () => {
+    alert("You Continue!");
+  };
+
   render() {
     const disableInfo = {
       ...this.state.ingredients,
@@ -97,7 +101,11 @@ class BurgerBuilder extends Component {
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+            ingredients={this.state.ingredients}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
