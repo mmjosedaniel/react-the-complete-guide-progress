84ffe534e2d883c060c2023684d2d084561cf667
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-03-07 20:05:36 -0500

    203. Extracting Route Parameters

--- http--01-starting-setup/src/containers/Blog/FullPost/FullPost.js
+++ http--01-starting-setup/src/containers/Blog/FullPost/FullPost.js
@@ -8,28 +8,25 @@ class FullPost extends Component {
     loadedPost: null,
   };
 
-  componentDidUpdate() {
-    if (this.props.id) {
+  componentDidMount() {
+    console.log(this.props);
+    if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
       ) {
-        axios
-          .get("/posts/" + this.props.id)
-          .then((response) => {
-            // console.log(response);
-            this.setState({ loadedPost: response.data });
-          });
+        axios.get("/posts/" + this.props.match.params.id).then((response) => {
+          // console.log(response);
+          this.setState({ loadedPost: response.data });
+        });
       }
     }
   }
 
   deletePostHandler = () => {
-    axios
-      .delete("/posts/" + this.props.id)
-      .then((response) => {
-        console.log(response);
-      });
+    axios.delete("/posts/" + this.props.id).then((response) => {
+      console.log(response);
+    });
   };
 
   render() {
