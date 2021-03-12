8c252d16ea31dcab50a031999d0d43c3d975fb47
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-03-11 19:38:25 -0500

    223. Navigating to the Checkout Page

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,6 +32,7 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
     axios
       .get(
         "https://react-my-burger-daniel-7229f-default-rtdb.firebaseio.com/ingredients.json"
@@ -96,29 +97,31 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert("You Continue!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Daniel Martinez",
-        address: {
-          street: "Test Street 1",
-          zipCode: "1234",
-          country: "Colombia",
-        },
-        email: "test@test.com",
-      },
-      deliberyMethod: "fastest",
-    };
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "Daniel Martinez",
+    //     address: {
+    //       street: "Test Street 1",
+    //       zipCode: "1234",
+    //       country: "Colombia",
+    //     },
+    //     email: "test@test.com",
+    //   },
+    //   deliberyMethod: "fastest",
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then((response) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+
+    this.props.history.push("/checkout");
   };
 
   render() {
