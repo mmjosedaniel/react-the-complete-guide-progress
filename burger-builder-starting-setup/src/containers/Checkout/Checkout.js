60d0d0b741fba6a7993adad554d21a84bdc0f088
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-03-15 12:52:38 -0500

    226. Navigating to the Contact Data Component

--- burger-builder-starting-setup/src/containers/Checkout/Checkout.js
+++ burger-builder-starting-setup/src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
   state = {
@@ -15,7 +17,7 @@ class Checkout extends Component {
   componentDidMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
-    
+
     for (let param of query.entries()) {
       // [salad, 1]
       ingredients[param[0]] = +param[1];
@@ -39,6 +41,7 @@ class Checkout extends Component {
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
         />
+        <Route path={this.props.match.path + "/contact-data"} component={ContactData} />
       </div>
     );
   }
