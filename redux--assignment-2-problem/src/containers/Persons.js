e276e1b8be5211b54bb0cca7764f651ef0bd39f2
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-03-24 12:47:40 -0500

    Add: add person with redux 'onAddPerson'

--- redux--assignment-2-problem/src/containers/Persons.js
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -3,21 +3,11 @@ import { connect } from "react-redux";
 
 import Person from "../components/Person/Person";
 import AddPerson from "../components/AddPerson/AddPerson";
+import * as actionTypes from "../store/actionTypes";
 
 class Persons extends Component {
   state = {
     persons: [],
-  };
-
-  personAddedHandler = () => {
-    const newPerson = {
-      id: Math.random(), // not really unique but good enough here!
-      name: "Max",
-      age: Math.floor(Math.random() * 40),
-    };
-    this.setState((prevState) => {
-      return { persons: prevState.persons.concat(newPerson) };
-    });
   };
 
   personDeletedHandler = (personId) => {
@@ -31,8 +21,8 @@ class Persons extends Component {
   render() {
     return (
       <div>
-        <AddPerson personAdded={this.personAddedHandler} />
-        {this.state.persons.map((person) => (
+        <AddPerson personAdded={this.props.onAddPerson} />
+        {this.props.persons.map((person) => (
           <Person
             key={person.id}
             name={person.name}
@@ -52,9 +42,9 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-
-    }
+  return {
+    onAddPerson: () => dispatch({ type: actionTypes.ADD_PERSON }),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Persons);
--- redux--assignment-2-problem/src/store/reducer.js
+++ redux--assignment-2-problem/src/store/reducer.js
@@ -5,7 +5,21 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.ADD_PERSON:
+      const newPerson = {
+        id: Math.random(), // not really unique but good enough here!
+        name: "Max",
+        age: Math.floor(Math.random() * 40),
+      };
+
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson),
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
