662bfa88c4efdb823ff96077a223bf7908607e9c
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-03-31 12:35:37 -0500

    310. Working on Order Actions

--- burger-builder-starting-setup/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-starting-setup/src/containers/Checkout/ContactData/ContactData.js
@@ -93,7 +93,6 @@ class ContactData extends Component {
       },
     },
     formIsValid: false,
-    loading: false,
   };
 
   orderHandler = (event) => {
@@ -186,7 +185,7 @@ class ContactData extends Component {
         </Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       from = <Spinner />;
     }
     return (
@@ -202,13 +201,13 @@ const mapStateToProps = (state) => {
   return {
     ings: state.ingredients,
     price: state.totalPrice,
+    loading: state.loading,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onOrderBurger: (orderData) =>
-      dispatch(actions.purchaseBurgerStart(orderData)),
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData)),
   };
 };
 
