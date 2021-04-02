10df4c54854bb717a105c7151a304eaea1f727ec
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2021-04-01 22:07:05 -0500

    312. Combining Reducers

--- burger-builder-starting-setup/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-starting-setup/src/containers/Checkout/ContactData/ContactData.js
@@ -87,7 +87,7 @@ class ContactData extends Component {
             { value: "cheapest", displayValue: "Cheapest" },
           ],
         },
-        value: "",
+        value: "fastes",
         validation: {},
         valid: true,
       },
@@ -199,9 +199,9 @@ class ContactData extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    loading: state.loading,
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading,
   };
 };
 
